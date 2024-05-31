d34655f2b659b0ebf005ec947383b1a8fc037504
dyson <dyson0326@docomo.ne.jp>
2024-05-31 23:22:12 +0900

    プロジェクトのセットアップ

---
+++ src/test/java/com/homework8/pokemon/tables/PokemonTablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework8.pokemon.tables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
